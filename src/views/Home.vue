d91075055ce983ef4ef1781ed02503b695f8109a
robin <robin.j.soto@gmail.com>
2020-10-12 14:02:39 -0700

    adds initial connections between frontend and backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h2>New Product</h2>
+      <button v-on:click="createProduct">Create product</button>
+    </div>
+    <h1>Products!</h1>
+    <div v-for="product in products">
+      <h2>Name: {{ product.name }}</h2>
+      <h4>Description: {{ product.description }}</h4>
+      <h5>Price: {{ product.price }}</h5>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      products: [],
+    };
+  },
+  created: function() {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function() {
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+    createProduct: function() {
+      var params = {
+        name: "Example name",
+        description: "Example description",
+        price: "Example price",
+      };
+
+      axios
+        .post("/api/products", params)
+        .then((response) => {
+          console.log("Success", response.data);
+          this.recipes.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+  },
+};
 </script>
